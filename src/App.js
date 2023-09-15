29da10fc5f58ef17b0e693cf7f94aa503cba5f9b
sarandevarjun <saransri13@gmail.com>
2023-09-15 11:24:21 +0530

    Added the initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(5);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setCounter((counterval) => counterval - 1)}>
+        Decrease the counter value-
+      </button>
+      <h1>App content here!{counter}</h1>
+      <button onClick={() => setCounter((counterval) => counterval + 1)}>
+        Increase the counter value+
+      </button>
+    </>
   );
-}
+};
 
 export default App;
